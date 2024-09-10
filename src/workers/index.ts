9afcc62f31e9b56dfa0e7ec37fa1f47d20751279
abc1230429 <abc1230429@gmail.com>
2024-09-10 17:03:12 +0800

    fix: worker memory leak

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -1,30 +1,25 @@
 import { Typo } from "src/types";
 
-export let findTyposWorker: Worker;
-
-export let findTyposAsync: (
+export const findTyposAsync: (
   article: string,
   refWord: string,
   options?: { threshold: number },
-) => Promise<Typo[]>;
-
-if (window.Worker) {
+) => Promise<Typo[]> = (article, refWord, options) => {
   const findTyposWorker = new Worker(
     new URL("./findTyposOfArticle.ts", import.meta.url),
     {
       type: "module",
     },
   );
-
-  findTyposAsync = (article, refWord, options) => {
-    return new Promise((resolve, reject) => {
-      findTyposWorker.postMessage([article, refWord, options]);
-      findTyposWorker.onmessage = function (e) {
-        resolve(e.data);
-      };
-      findTyposWorker.onerror = function (e) {
-        reject(e);
-      };
-    });
-  };
-}
+  return new Promise((resolve, reject) => {
+    findTyposWorker.postMessage([article, refWord, options]);
+    findTyposWorker.onmessage = function (e) {
+      resolve(e.data);
+      findTyposWorker.terminate();
+    };
+    findTyposWorker.onerror = function (e) {
+      reject(e);
+      findTyposWorker.terminate();
+    };
+  });
+};
