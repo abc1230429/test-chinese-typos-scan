bce5664542bb9841db8af608d6f7a9a96e5385b1
abc1230429 <abc1230429@gmail.com>
2024-09-11 20:50:36 +0800

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/test-chinese-typos-scan/",
   plugins: [react(), visualizer()],
   resolve: {
     alias: {
